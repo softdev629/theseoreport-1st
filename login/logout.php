f0299d4b3a7a870492c3cff56c80336385e99423
Iheb <iheb.draouil@outlook.com>
2022-12-14 22:49:41 +0100

    site-wide vendasta sso

--- login/logout.php
+++ login/logout.php
@@ -2,8 +2,6 @@
 
 require_once __DIR__ . '/includes/common.php';
 
-if (array_key_exists($COOKIE_NAME, $_COOKIE) && $_COOKIE[$COOKIE_NAME] != '') {
-    setcookie($COOKIE_NAME, '');
-}
+setcookie($COOKIE_NAME, '');
 
 header("Location: $LOGIN_PAGE_PATH");
